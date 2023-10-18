4be383824a2478e8d00a76f64e6f9e5fd61625d2
ZwingliCaleb <calebzwingli@gmail.com>
2023-10-18 09:50:02 +0300

    Install and Use react Router for navigation

---
+++ src/ProfilePage/ProfilePage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+
+const ProfilePage = () => {
+  const location = useLocation();
+  const consolidatedLinks = location.state ? location.state.links : [];
+
+  return (
+    <div>
+      <h1>Your Profile Page</h1>
+      {consolidatedLinks.map((link, index) => (
+        <div key={index}>
+          <p>Website Name: {link.websiteName}</p>
+          <p>Description: {link.description}</p>
+          <p>URL: {link.url}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ProfilePage;
