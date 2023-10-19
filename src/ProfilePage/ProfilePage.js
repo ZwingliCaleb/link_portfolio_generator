50e157896f2125460cd3ef02b887722171e2aa8d
ZwingliCaleb <calebzwingli@gmail.com>
2023-10-19 17:05:23 +0300

    Style profilepage

--- src/ProfilePage/ProfilePage.js
+++ src/ProfilePage/ProfilePage.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+import './ProfilePage.css';
 
 const ProfilePage = () => {
   const location = useLocation();
+  const Navigate = useNavigate();
   const consolidatedLinks = location.state ? location.state.links : [];
   console.log(consolidatedLinks);
+
+  const handleBackClick = () => {
+    Navigate(-1);
+  };
 
   return (
     <div>
       <h1>Your Profile Page</h1>
+      <button onClick={handleBackClick}>Back to form</button>
+
       {consolidatedLinks.map((link, index) => (
         <div key={index}>
           <p>Website Name: {link.websiteName}</p>
@@ -16,6 +24,11 @@ const ProfilePage = () => {
           <p>URL: {link.url}</p>
         </div>
       ))}
+
+      <div className="long-link-box">
+        <input type="text" value="YOUR_PROFILE_LINK_HERE" radonly />
+        <button onClick={() => {}}>Copy</button>
+      </div>
     </div>
   );
 };
